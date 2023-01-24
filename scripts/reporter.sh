705edd3aa14228589704b99a6342f5e283e7e073
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-24 08:41:31 +0100

    test(codacy): update scripts/reporter.sh

--- scripts/reporter.sh
+++ scripts/reporter.sh
@@ -8,10 +8,10 @@ printf "# CODACY - COVERAGE REPORTER #\n"
 printf "##############################\n"
 
 CODACY_PROJECT_TOKEN_FILE=./scripts/CODACY_PROJECT_TOKEN.sh
-LCOV_INFO_FILE=./coverage/app/lcov.info
+LCOV_INFO_FILE=./coverage/lcov/lcov.info
 
-if [ -f ${CODACY_PROJECT_TOKEN_FILE} ]; then
-  source ${CODACY_PROJECT_TOKEN_FILE}
+if [ -f "${CODACY_PROJECT_TOKEN_FILE}" ]; then
+  source "${CODACY_PROJECT_TOKEN_FILE}"
 fi
 
 if [ "${CODACY_PROJECT_TOKEN}" == "" ]; then
@@ -20,7 +20,10 @@ if [ "${CODACY_PROJECT_TOKEN}" == "" ]; then
 else
   printf "> API token CODACY_PROJECT_TOKEN = %s\n" "${CODACY_PROJECT_TOKEN}"
   printf "> Generate code coverage:\n"
+
   docker compose exec node ng test --no-watch --code-coverage
+  git add "${LCOV_INFO_FILE}"
+  git commit -m "tests(codacy): save coverage reports in lcov format"
 
   if [ -f ${LCOV_INFO_FILE} ]; then
     printf "> Upload the coverage reports '%s'\n" "${LCOV_INFO_FILE}"
@@ -28,4 +31,6 @@ else
   else
     printf "ERROR! The file '%s' does not exist.\n" "${LCOV_INFO_FILE}"
   fi
+
+  printf "> Update remote refs with '$ git push'.\n"
 fi

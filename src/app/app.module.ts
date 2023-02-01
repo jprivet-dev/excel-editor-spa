75226e3c23c31b23119fa9e03ca60eff19bdecd1
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-01 11:04:25 +0100

    build(ng): add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CoreModule } from '@core/core.module';
 import { SharedModule } from '@shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     CoreModule,
     SharedModule,
+    NoopAnimationsModule,
   ],
   providers: [
     {

a1d8f69d1a2f0189a989040152ca9579eb3736a0
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-28 18:17:07 +0100

    style(all): adjust some styles

--- src/app/modules/data/data.component.ts
+++ src/app/modules/data/data.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
+  styleUrls: ['./data.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DataComponent {}
